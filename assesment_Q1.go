96208bbf90c0fead5711b54931c4127cf78ab657
Ravi80195 <111380041+Ravi80195@users.noreply.github.com>
2022-09-08 11:49:35 -0500

    Update assesment_Q1.go

--- assesment_Q1.go
+++ assesment_Q1.go
@@ -1,24 +1,24 @@
+//1 . Write a function with this signature:
 
-	//1 . Write a function with this signature:
-	
-  
 package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
-	"unicode"
 )
-//Below method would transform the the input rune to the expected output.
-//It has logic implemented where we have logic to check if it is a unicode character and if it is a unicode character then we check for the position.
-//We check for the position for every third character ignoring the special characters in previous condition.
-//If the 3 position is found we capitalize the character at that particular position.
-//We do it in a similar way for all the characters.
+
+// Below method would transform the the input rune to the expected output.
+// It has logic implemented where we have logic to check if it is a unicode character and if it is a unicode character then we check for the position.
+// We check for the position for every third character ignoring the special characters in previous condition.
+// If the 3 position is found we capitalize the character at that particular position.
+// We do it in a similar way for all the characters.
 func CapitalizeEveryThirdAlphanumericChar(position int, input string) string {
 	input = strings.ToLower(input)
 	charIndex := 1
+	re := regexp.MustCompile("^[a-z0-9]*$")
 	for pos := range []rune(input) {
-		if unicode.IsLetter(rune(input[pos])) {
+		if re.MatchString(string(input[pos])) {
 			if charIndex%position == 0 {
 				input = input[:pos] + strings.ToUpper(string(input[pos])) + input[pos+1:]
 			}
@@ -31,7 +31,7 @@ func CapitalizeEveryThirdAlphanumericChar(position int, input string) string {
 
 // And change your code to look like this:
 func main() {
-	s := CapitalizeEveryThirdAlphanumericChar(3, "Aspiration.com")
+	s := CapitalizeEveryThirdAlphanumericChar(3, "As3piration.com")
 	fmt.Println(s)
 }
 
@@ -39,7 +39,7 @@ func main() {
 // 	Aspiration.com
 // 	You hand me back
 // 	asPirAtiOn.cOm
-// 	Please note: 
+// 	Please note:
 // 	- Characters other than each third should be downcased
 // 	- For the purposes of counting and capitalizing every three characters, consider only alphanumeric
-// 	  characters, ie [a-z][A-Z][0-9]. 
+// 	  characters, ie [a-z][A-Z][0-9].
